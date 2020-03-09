29652c1826e304a9b8ca3c4b08c402219b10f600
Satvika Eda <satvika164.reddy@gmail.com>
2020-03-09 22:25:58 +0530

    Add files via upload

---
+++ Implementation.java
@@ -0,0 +1,6 @@
+import java.util.*;
+class Implementation{
+  public static List myList(Object[] a) {
+     return new ArrayList(a);
+  }
+}
---
+++ collectionsImplementation.java
@@ -0,0 +1,27 @@
+public class collectionsImplementation{
+ public static void main(String[] args) {
+      try{
+    String s[]={"My", "Custom", "Implementation","for","first","time","on","giving","a","project"};
+    Object o;
+    int i=0;
+    Implementation a= new Implementation();
+    List lst=a.myList(s);
+    System.out.println("The list is: "+lst);
+    ArrayList al=new ArrayList(s);
+    Scanner sin=new Scanner(System.in);
+    System.out.println("enter the elements position to be fetched");
+    int z=sin.nextInt();
+    o=al.get(z);
+    System.out.println("The fetched element is: "+o);
+    String s1="Collection";
+    o=al.set(6,s1);
+    System.out.println("The set element in place of Implementation is: "+s1);
+    System.out.println("Now the new list is: "+lst);
+    i=al.size();
+    System.out.println("The size of the array list is: "+i);
+    }
+    catch(Exception e){
+    	System.out.println("exception caught");
+    }
+  }
+  }
